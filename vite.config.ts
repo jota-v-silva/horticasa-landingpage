e341eac8de451ea419672a48d7cb9d2ed942b797
Joao Silva <jota.vasconceloss@gmail.com>
2025-12-03 21:51:00 +0000

    Updates vite related configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/horticasa-landingpage/',
   server: {
     host: "::",
     port: 8080,
